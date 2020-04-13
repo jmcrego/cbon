e39c43b63a2c01e5f18546b801a5990424627392
jmcrego <jmcrego@gmail.com>
2020-04-13 14:12:51 +0200

    add

--- run.sh
+++ run.sh
@@ -2,18 +2,20 @@
 
 download(){
     mkdir -p $DIR
-    wget http://data.statmt.org/news-commentary/v14/training/news-commentary-v14.en-fr.tsv.gz
-    mv news-commentary-v14.en-fr.tsv.gz $DIR/
-    gunzip $DIR/news-commentary-v14.en-fr.tsv.gz
-    cut -f 1 $DIR/news-commentary-v14.en-fr.tsv > $DIR/news-commentary-v14.en
-    cut -f 2 $DIR/news-commentary-v14.en-fr.tsv > $DIR/news-commentary-v14.fr
-    rm -f $DIR/news-commentary-v14.en-fr.tsv
+	rsync -avzP ssaling02:~/raw/Europarl/enfr/Europarl.en-fr.en $DIR/
+	rsync -avzP ssaling02:~/raw/ECB/enfr/ECB.en-fr.en $DIR/
+	rsync -avzP ssaling02:~/raw/EMEA/enfr/EMEA.en-fr.en $DIR/
+	rsync -avzP ssaling02:~/raw/news-commentary/enfr/news-commentary-v14.en $DIR/
+	rsync -avzP ssaling02:~/raw/JRC-Acquis/enfr/JRC-Acquis.en-fr.en $DIR/
+	rsync -avzP ssaling02:~/raw/TED2013/enfr/TED2013.en-fr.en $DIR/
+	rsync -avzP ssaling02:~/raw/Wikipedia/enfr/Wikipedia.en-fr.en $DIR/
     echo -e "mode: conservative\njoiner_annotate: true" > $DIR/token_options
 }
 
 DIR=$PWD/raw
 
-#download
+#
+download
 #python3 cbon.py -name ex1 -mode preprocess -data $DIR/news-commentary-v14.en -voc_maxn 3 -voc_minf 10 -tok_conf $DIR/token_options -log_file stderr
 #python3 cbon.py -name ex1 -mode train -data $DIR/news-commentary-v14.en -log_file stderr -skip_subsampling -embedding_size 100 -batch_size 8
 
