a70f22aa9baf9ab86211f99fb4dbe6fd4c305e4f
jmcrego <jmcrego@gmail.com>
2020-04-15 12:54:08 +0200

    random batches

--- run.sh
+++ run.sh
@@ -16,9 +16,16 @@ download(){
 DIR=$PWD/raw
 
 #download
+
 #python3 cbon.py -name ex1 -mode preprocess -data $DIR/\*.en -voc_maxn 3 -voc_minf 10 -voc_maxs 500000 -tok_conf $DIR/token_options -log_file stderr
-#CUDA_VISIBLE_DEVICES=$gpu python3 cbon.py -name ex1 -mode train -data $DIR/\*.en -skip_subsampling -embedding_size 100 -batch_size 1024 -cuda -log_file stderr
+#CUDA_VISIBLE_DEVICES=$gpu python3 cbon.py -name ex1 -mode train -data $DIR/\*.en -skip_subsampling -embedding_size 100 -batch_size 2048 -cuda
 
+#cp ex1.vocab ex1b.vocab
+#cp ex1.token ex1b.token
+#
+CUDA_VISIBLE_DEVICES=$gpu python3 cbon.py -name ex1b -mode train -data $DIR/\*.en -skip_subsampling -embedding_size 100 -batch_size 2048 -cuda -window 0 &
 
-python3 cbon.py -name ex2 -mode preprocess -data $DIR/\*.en -voc_maxn 3 -voc_minf 10 -voc_maxs 1000000 -tok_conf $DIR/token_options -log_file stderr
-#CUDA_VISIBLE_DEVICES=$gpu python3 cbon.py -name ex2 -mode train -data $DIR/\*.en -skip_subsampling -embedding_size 100 -batch_size 1024 -cuda -log_file stderr
+#python3 cbon.py -name ex2 -mode preprocess -data $DIR/\*.en -voc_maxn 3 -voc_minf 10 -voc_maxs 1000000 -tok_conf $DIR/token_options -log_file stderr
+#
+CUDA_VISIBLE_DEVICES=$gpu python3 cbon.py -name ex2 -mode train -data $DIR/\*.en -skip_subsampling -embedding_size 100 -batch_size 2048 -cuda &
+
