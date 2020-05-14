c1eb5efeb584e713a1e2ece3b44e33ba86aea432
jmcrego <jmcrego@gmail.com>
2020-05-14 12:51:49 +0200

    shard

--- dataset.py
+++ dataset.py
@@ -57,7 +57,7 @@ class Dataset():
         #    logging.info('subsampled to {} tokens'.format(ntokens))
 
 
-    def get_ctx_neg(self, idxs, center, do_neg=False):
+    def get_ctx_neg(self, idxs, center):
         toks = [self.vocab[idxs[i]] for i in range(len(idxs))]
         #logging.info('center={}:{} {}'.format(center, toks[center], ' '.join(toks)))
 
@@ -103,53 +103,41 @@ class Dataset():
         ### find Negative words
         ###
         neg = []
-        while do_neg and len(neg) < self.n_negs:
+        while len(neg) < self.n_negs:
             idx = random.randint(2, self.vocab_size-2) #do not consider idx=0 (pad) nor idx=1 (unk)
             if idx != idxs[center] and idx not in ctx:
                 neg.append(idx)
 
         return ctx, neg
 
-    def get_context(self, toks, center=None):
-        ctx = []
-        msk = [] #mask of positive words (to indicate true words 1.0 or padding 0.0)
-        if self.window > 0 and center is not None:
-            first_idx = max(center-self.window, 0)
-            last_idx = min(center+self.window, len(toks)-1)
-        else:
-            first_idx = 0
-            last_idx = len(toks)-1
-
-        ### add all ngrams in [first_idx, last_idx] which do not contain center (unless center==None)
-        for i in range(first_idx, last_idx+1):
-            if center is not None and i == center:
+    def get_snt(self, idxs):
+        toks = [self.vocab[idxs[i]] for i in range(len(idxs))]
+        #logging.info('center={}:{} {}'.format(center, toks[center], ' '.join(toks)))
+        beg = 0
+        end = len(idxs)
+        ###
+        ### find all ngrams
+        ###
+        snt = []
+        for i in range(beg, end): 
+            if idxs[i] == self.idx_unk:
                 continue
-            ctx.append(toks[i]) #toks[i] = 34
-            msk.append(True)
-            ngrams = [str(toks[i])] #ngrams ['34']
+            self.total_ngrams[1] += 1
+            snt.append(idxs[i])
+            #logging.info('[{}] 1-gram_idx={} \'{}\''.format(i, idxs[i], toks[i]))
+            for j in range(i+2,i+self.voc_maxn+1): 
+                if j > len(idxs):
+                    break
+                if self.idx_unk in idxs[i:j]:
+                    break
+                ngram = ' '.join([str(k) for k in idxs[i:j]])
+                idx = self.vocab[ngram]
+                if idx != self.idx_unk:
+                    snt.append(idx)
+                    self.total_ngrams[j-i] += 1
+                    #logging.info('[{}:{}) {}-gram_idx={} \'{}\''.format(i, j, j-i, idx, ngram))
+        return snt
 
-            for n in range(1,self.voc_maxn):
-                if center is not None and i+n == center:
-                    break
-                if i+n >= len(toks):
-                    break
-                if toks[i+n] == self.idx_unk:
-                    break
-                ngrams.append(str(toks[i+n])) #ngrams ['34', '28']
-                ngram = self.vocab[' '.join(ngrams)] #ngram = 124
-                if ngram == self.idx_unk:
-                    break
-                ctx.append(ngram)
-                msk.append(True)
-        return ctx, msk
-
-    def get_neg(self, wrd, ctx):
-        neg = []
-        while len(neg) < self.n_negs:
-            idx = random.randint(2, self.vocab_size-2) #do not consider idx=0 (pad) nor idx=1 (unk)
-            if idx != wrd and idx not in ctx:
-                neg.append(idx)
-        return neg
 
     def add_pad(self, batch_ctx, batch_msk):
         max_len = max([len(x) for x in batch_ctx])
@@ -160,6 +148,10 @@ class Dataset():
             batch_msk[k] += [False]*addn
             #print(len(batch_ctx[k]),batch_ctx[k])
         return batch_ctx, batch_msk
+
+
+
+
 
     def __iter__(self):
         ######################################################
@@ -182,43 +174,15 @@ class Dataset():
         ### sentence-vectors #################################
         ######################################################
         elif self.mode == 'sentence-vectors':
-            ### build indexs
             self.indexs = [i for i in range(len(self.corpus))]
-            ### shuffle indexs
-            random.shuffle(self.indexs)
+            #no need to shuffle! random.shuffle(self.indexs)
             first_index = 0
             while first_index < len(self.indexs):
-                next_index = min(first_index + self.shard_size, len(self.indexs))
-                indexs_shard = self.indexs[first_index:next_index] ### a bunch of indexs of self.corpus
-                first_index = next_index
-                ### this shard is built of indexs_shard, indexes that points to self.indexs
-                logging.info('sorting examples in shard by length')
-                length = [len(self.corpus[ind]) for ind in indexs_shard] #length of sentences in this shard
-                indexs = np.argsort(np.array(length)) ### These are indexs of indexs_shard which are indexs of self.corpus
-
-                batchs = []
-                batch_snt = []
-                batch_msk = []
-                batch_ind = []
-                for ind in indexs:
-                    snt, msk = self.get_context(self.corpus[indexs_shard[ind]]) ### returns context for the entire sentence
-                    batch_snt.append(snt)
-                    batch_msk.append(msk)
-                    batch_ind.append(indexs_shard[ind]) ### position in original corpus
-                    ### batch filled
-                    if len(batch_snt) == self.batch_size:
-                        batch_snt, batch_msk = self.add_pad(batch_snt, batch_msk)
-                        batchs.append([batch_snt, batch_msk, batch_ind])
-                        batch_snt = []
-                        batch_msk = []
-                        batch_ind = []
-                if len(batch_snt):
-                    batch_snt, batch_msk = self.add_pad(batch_snt, batch_msk)
-                    batchs.append([batch_snt, batch_msk, batch_ind])
-
-                logging.info('compiled {} batchs using {} examples'.format(len(batchs), len(indexs_shard)))
+                indexs_shard, first_index = self.get_shard(first_index) # indexs_shard contains a subset of the indexs stored in self.indexs
+                examples = self.get_examples(indexs_shard)
+                batchs = self.get_batchs_snt(examples)
                 indexs_batchs = [i for i in range(len(batchs))]
-                random.shuffle(indexs_batchs)
+                #no need to shuffle! random.shuffle(indexs_batchs)
                 for ind in indexs_batchs:
                     yield batchs[ind]
 
@@ -226,78 +190,17 @@ class Dataset():
         ### train ############################################
         ######################################################
         elif self.mode == 'train':
-            ### build indexs
-            indexs = [i for i in range(len(self.corpus))]
-            ### shuffle indexs
-            random.shuffle(indexs)
-
+            self.indexs = [i for i in range(len(self.corpus))]
+            random.shuffle(self.indexs) #shuffle self.indexs
             first_index = 0
-            while first_index < len(indexs):
-                next_index = min(first_index + self.shard_size, len(indexs))
-                indexs_shard = indexs[first_index:next_index]
-                first_index = next_index
-                ### indexs_shard contains a subset of the indexes stored in self.indexs
-                logging.info('built shard with {} out of {} sentences'.format(len(indexs_shard),len(indexs)))
-                examples = [] ### ind (position in corpus), wrd (word to predict or empty), neg (n negative words or empty), ctx (context or sentence)
-                for ind in indexs_shard:
-                    for center in range(len(self.corpus[ind])):
-                        wrd = self.corpus[ind][center] #idx
-                        if wrd < 2: ### i dont want to predict <unk> or <pad>
-                            continue
-                        ctx, neg = self.get_ctx_neg(self.corpus[ind], center, True) #[idx, idx, ...], [idx, idx, ...]
-                        if len(ctx)==0 or len(neg)==0:
-                            continue
-                        e = []
-                        #logging.info('wrd: {}'.format(wrd))
-                        #logging.info('ctx: {}'.format(ctx))
-                        #logging.info('neg: {}'.format(neg))
-                        e.append(wrd) #the word to predict
-                        e.extend(neg) #n_negs negative words
-                        e.extend(ctx) #ngrams around [center-window, center+window] used to predict
-                        examples.append(e)
-                logging.info('compiled shard with {} examples'.format(len(examples)))
-                for n,N in self.total_ngrams.items():
-                    logging.info('ctx {}-grams: {}'.format(n,N))
-
-                ### sort examples by len
-                logging.info('sorting examples in shard by number of ngrams to minimize padding')
-                length = [len(examples[k]) for k in range(len(examples))] #length of sentences in this shard
-                index_examples = np.argsort(np.array(length)) ### These are indexs of examples
-
-                batch = []
-                batch_wrd = []
-                batch_ctx = []
-                batch_neg = []
-                batch_msk = []
-                for ind in index_examples:
-                    e = examples[ind]
-                    wrd = e[0]
-                    neg = e[1:self.n_negs+1]
-                    ctx = e[self.n_negs+1:]
-                    #if len(ctx) == 0:
-                    #    logging.error('ctx length is 0')
-                    #    sys.exit()
-                    msk = [True] * len(ctx)
-                    batch_wrd.append(wrd)
-                    batch_ctx.append(ctx)
-                    batch_neg.append(neg)
-                    batch_msk.append(msk)
-                    if len(batch_wrd) == self.batch_size:
-                        batch_ctx, batch_msk = self.add_pad(batch_ctx, batch_msk)
-                        batch.append([batch_wrd, batch_ctx, batch_neg, batch_msk])
-                        batch_wrd = []
-                        batch_ctx = []
-                        batch_neg = []
-                        batch_msk = []
-                if len(batch_wrd):
-                    batch_ctx, batch_msk = self.add_pad(batch_ctx, batch_msk)
-                    batch.append([batch_wrd, batch_ctx, batch_neg, batch_msk])
-                logging.info('found {} batchs in shard'.format(len(batch)))
-
-                indexs_batchs = [i for i in range(len(batch))]
-                random.shuffle(indexs_batchs)
+            while first_index < len(self.indexs):
+                indexs_shard, first_index = self.get_shard(first_index) # indexs_shard contains a subset of the indexes stored in self.indexs
+                examples = self.get_examples(indexs_shard)
+                batchs = self.get_batchs(examples)
+                indexs_batchs = [i for i in range(len(batchs))]
+                random.shuffle(indexs_batchs) #shuffle indexs_batchs
                 for ind in indexs_batchs:
-                    yield batch[ind]
+                    yield batchs[ind]
 
         ######################################################
         ### error ############################################
@@ -305,6 +208,123 @@ class Dataset():
         else:
             logging.error('bad -mode option {}'.format(self.mode))
             sys.exit()
+
+
+    def get_shard(self, first_index):
+        next_index = min(first_index + self.shard_size, len(self.indexs))
+        indexs_shard = self.indexs[first_index:next_index]
+        logging.info('built shard with {} out of {} sentences [{}, {})'.format(len(indexs_shard), len(self.indexs), first_index, next_index))
+        first_index = next_index
+        return indexs_shard, first_index
+
+
+    def get_examples(self, indexs_shard):
+        examples = [] ### ind (position in corpus), wrd (word to predict or empty), neg (n negative words or empty), ctx (context or sentence)
+        for ind in indexs_shard:
+            for center in range(len(self.corpus[ind])):
+                wrd = self.corpus[ind][center] #idx
+                if wrd < 2: ### i dont want to predict <unk> or <pad>
+                    continue
+                ctx, neg = self.get_ctx_neg(self.corpus[ind], center) #[idx, idx, ...], [idx, idx, ...]
+                if len(ctx)==0:
+                    continue
+                e = []
+                #logging.info('wrd: {}'.format(wrd))
+                #logging.info('ctx: {}'.format(ctx))
+                #logging.info('neg: {}'.format(neg))
+                e.append(wrd) #the word to predict
+                e.extend(neg) #n_negs negative words
+                e.extend(ctx) #ngrams around [center-window, center+window] used to predict
+                examples.append(e)
+        logging.info('compiled shard with {} examples'.format(len(examples)))
+        for n,N in self.total_ngrams.items():
+            logging.info('ctx {}-grams: {}'.format(n,N))
+        return examples
+
+    def get_examples_snt(self, indexs_shard):
+        examples = [] ### ind (position in corpus), wrd (word to predict or empty), neg (n negative words or empty), ctx (context or sentence)
+        for ind in indexs_shard:
+            snt = self.get_snt(self.corpus[ind]) #[idx, idx, ...], [i]
+            if len(snt)==0: # at least one ngram in snt
+                continue
+            e = []
+            #logging.info('snt: {}'.format(snt))
+            e.append(ind) #position in corpus
+            e.extend(snt) #ngrams in sentence
+            examples.append(e)
+        logging.info('compiled shard with {} examples'.format(len(examples)))
+        for n,N in self.total_ngrams.items():
+            logging.info('ctx {}-grams: {}'.format(n,N))
+        return examples
+
+
+    def get_batchs(self, examples):
+        ### sort examples by len
+        logging.info('sorting examples in shard by number of ngrams to minimize padding')
+        length = [len(examples[k]) for k in range(len(examples))] #length of sentences in this shard
+        index_examples = np.argsort(np.array(length)) ### These are indexs of examples
+
+        batchs = []
+        batch_wrd = []
+        batch_ctx = []
+        batch_neg = []
+        batch_msk = []
+        for ind in index_examples:
+            e = examples[ind]
+            wrd = e[0]
+            neg = e[1:self.n_negs+1]
+            ctx = e[self.n_negs+1:]
+            #if len(ctx) == 0:
+            #    logging.error('ctx length is 0')
+            #    sys.exit()
+            msk = [True] * len(ctx)
+            batch_wrd.append(wrd)
+            batch_ctx.append(ctx)
+            batch_neg.append(neg)
+            batch_msk.append(msk)
+            if len(batch_wrd) == self.batch_size:
+                batch_ctx, batch_msk = self.add_pad(batch_ctx, batch_msk)
+                batchs.append([batch_wrd, batch_ctx, batch_neg, batch_msk])
+                batch_wrd = []
+                batch_ctx = []
+                batch_neg = []
+                batch_msk = []
+        if len(batch_wrd):
+            batch_ctx, batch_msk = self.add_pad(batch_ctx, batch_msk)
+            batchs.append([batch_wrd, batch_ctx, batch_neg, batch_msk])
+        logging.info('found {} batchs in shard'.format(len(batchs)))
+        return batchs
+
+
+    def get_batchs_snt(self, examples):
+        ### this shard is built of indexs_shard, indexes that points to self.indexs
+        logging.info('sorting examples in shard by number of ngrams to minimize padding')
+        length = [len(examples[k]) for k in range(len(examples))] #length of sentences in this shard
+        index_examples = np.argsort(np.array(length)) ### These are indexs of examples
+
+        batchs = []
+        batch_snt = []
+        batch_msk = []
+        batch_ind = []
+        for ind in index_examples:
+            e = examples[ind]
+            ind = e[0]
+            snt = e[1:]
+            msk = [True] * len(snt)
+            batch_ind.append(ind) ### position in original corpus
+            batch_snt.append(snt)
+            batch_msk.append(msk)
+            ### batch filled
+            if len(batch_snt) == self.batch_size:
+                batch_snt, batch_msk = self.add_pad(batch_snt, batch_msk)
+                batchs.append([batch_snt, batch_msk, batch_ind])
+                batch_snt = []
+                batch_msk = []
+                batch_ind = []
+        if len(batch_snt):
+            batch_snt, batch_msk = self.add_pad(batch_snt, batch_msk)
+            batchs.append([batch_snt, batch_msk, batch_ind])
+        return batchs
 
 
     def SubSample(self, sum_counts):
